27858ae66274305adac186bdf3fa33cf7ffe5afa
KASHAN-HAMID <abrajalrayansupermarket2@gmail.com>
2025-06-25 03:26:56 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className="w-screen h-screen bg-white m-0 p-0 overflow-hidden">
+    <section className="w-screen h-screen bg-white m-0 p-0 flex items-center justify-center overflow-hidden">
       <img
         src="/portfolio/10.jpg"
         alt="Hero"
-        className="w-full h-full object-cover"
+        className="w-full h-full object-contain"
       />
     </section>
   );
