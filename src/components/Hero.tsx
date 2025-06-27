6c822e81c3445793f67f681829fbd1819ef73cbc
KASHAN-HAMID <abrajalrayansupermarket2@gmail.com>
2025-06-26 21:42:58 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -77,13 +77,13 @@ const Hero = () => {
       {/* Centered Content */}
       <div className="flex flex-1 items-center justify-center text-center px-6 z-10 mt-56">
         <div>
-          <h2 className="text-blue-600 text-2xl md:text-3xl font-semibold mb-2">
-            Graphic Design
+         <p className="text-blue-500 text-lg md:text-xl mt-2 font-medium">
+            Ahmed Saleem
           </h2>
           <h1 className="text-black text-[72px] md:text-[100px] lg:text-[120px] font-extrabold leading-none tracking-tight">
             portfolio
           </h1>
-          <p className="text-blue-600 text-lg md:text-xl mt-2 font-medium">
+          <p className="text-blue-500 text-lg md:text-xl mt-2 font-medium">
             Social Media Graphic Design
           </p>
         </div>
